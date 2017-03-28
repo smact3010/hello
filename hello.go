c94436eab16a224c10d8a1ffec37c061cf260f75
Stephan Taylor <stephan.taylor@irco.com>
2017-03-28 15:19:01 -0600

    second commit

--- hello.go
+++ hello.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+
+    "github.com/smact3010/stringutil"
+)
 
 func main() {
     fmt.Printf("hello, world\n")
+
+    fmt. Printf(stringutil.Reverse("!oG ,olleH"))
 }
